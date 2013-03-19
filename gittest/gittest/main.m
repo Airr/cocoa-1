63cc7f62117eb62ca27a9b272507972a8967e855
Sang Chul Choi <goshng@lemming.iab.alaska.edu>
2013-03-19 15:04:47 -0800

    An initial commit.

---
+++ gittest/gittest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by Sang Chul Choi on 3/19/13.
+//  Copyright (c) 2013 Sang Chul Choi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
