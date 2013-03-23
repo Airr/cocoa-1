27fd8a433ff6cdad91d714692b1ffccc92baa056
Sang Chul Choi <goshng@yahoo.co.kr>
2013-03-22 21:45:00 -0800

    After the first round of completing SpeakLine.

--- SpeakLine/SpeakLine/SpeakLineAppDelegate.m
+++ SpeakLine/SpeakLine/SpeakLineAppDelegate.m
@@ -6,23 +6,32 @@
 //  Copyright (c) 2013 Sang Chul Choi. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
 #import "SpeakLineAppDelegate.h"
 
 @implementation SpeakLineAppDelegate
 
 @synthesize window = _window;
 @synthesize textField = _textField;
-
+@synthesize tableView = _tableView;
 - (id)init
 {
     self = [super init];
     if (self) {
         _speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+        [_speechSynth setDelegate:self];
+        _voices = [NSSpeechSynthesizer availableVoices];
     }
     return self;
 }
 
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender
+        didFinishSpeaking:(BOOL)finishedSpeaking
+{
+    NSLog(@"finishedSpeaking = %d", finishedSpeaking);
+    [_stopButton setEnabled:NO];
+    [_speakButton setEnabled:YES];
+    [_tableView setEnabled:YES];
+}
 
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
@@ -43,5 +52,44 @@
         return; }
     [_speechSynth startSpeakingString:string];
     NSLog(@"Have started to say: %@", string);
+    [_stopButton setEnabled:YES];
+    [_speakButton setEnabled:NO];
+    [_tableView setEnabled:NO];
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tv
+{
+    return (NSInteger)[_voices count];
+}
+
+- (id)tableView:(NSTableView *)tv
+objectValueForTableColumn:(NSTableColumn *)tableColumn
+            row:(NSInteger)row
+{
+    NSString *v = [_voices objectAtIndex:row];
+    NSDictionary *dict = [NSSpeechSynthesizer attributesForVoice:v];
+    return [dict objectForKey:NSVoiceName];
+}
+
+- (void)tableViewSelectionDidChange:(NSNotification *)notification
+{
+    NSInteger row = [_tableView selectedRow];
+    if (row == -1) {
+        return;
+    }
+    NSString *selectedVoice = [_voices objectAtIndex:row];
+    [_speechSynth setVoice:selectedVoice];
+    NSLog(@"new voice = %@", selectedVoice);
+}
+
+- (void)awakeFromNib
+{
+    // When the table view appears on screen, the default voice
+    // should be selected
+    NSString *defaultVoice = [NSSpeechSynthesizer defaultVoice];
+    NSInteger defaultRow = [_voices indexOfObject:defaultVoice];
+    NSIndexSet *indices = [NSIndexSet indexSetWithIndex:defaultRow];
+    [_tableView selectRowIndexes:indices byExtendingSelection:NO];
+    [_tableView scrollRowToVisible:defaultRow];
 }
 @end
