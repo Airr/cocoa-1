b3a76b1bc3fb733db76a3a16b13014d88ba50a67
Sang Chul Choi <goshng@yahoo.co.kr>
2013-03-27 15:26:45 -0800

    Cocoa programming a quick start guide for developers - Chapter "Introduction"

---
+++ SimpleBrowser/SimpleBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleBrowser
+//
+//  Created by Sang Chul Choi on 3/27/13.
+//  Copyright (c) 2013 Sang Chul Choi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SimpleBrowser/SimpleBrowser/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SimpleBrowser
+//
+//  Created by Sang Chul Choi on 3/27/13.
+//  Copyright (c) 2013 Sang Chul Choi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
