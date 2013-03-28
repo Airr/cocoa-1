f078551812495f4b19f9c72170ea4340d4f2d2b4
Sang Chul Choi <goshng@yahoo.co.kr>
2013-03-28 14:22:37 -0800

    Chapter Cocoa Bindings of Cocoa programming a quick start guide for developers

---
+++ CounterWithBindings/CounterWithBindings/Counter.h
@@ -0,0 +1,13 @@
+//
+//  Counter.h
+//  CounterWithBindings
+//
+//  Created by Sang Chul Choi on 3/28/13.
+//  Copyright (c) 2013 Sang Chul Choi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Counter : NSObject
+@property (copy) NSNumber *count;
+@end
---
+++ CounterWithBindings/CounterWithBindings/Counter.m
@@ -0,0 +1,13 @@
+//
+//  Counter.m
+//  CounterWithBindings
+//
+//  Created by Sang Chul Choi on 3/28/13.
+//  Copyright (c) 2013 Sang Chul Choi. All rights reserved.
+//
+
+#import "Counter.h"
+
+@implementation Counter
+
+@end
