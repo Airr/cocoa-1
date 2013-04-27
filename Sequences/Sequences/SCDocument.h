c1114ad0197a9d3e26356c309d136cc6ca7c13cf
Sang Chul Choi <goshng@yahoo.co.kr>
2013-04-27 10:53:51 -0800

    The initial commit of Sequences for handling biological sequences.

---
+++ Sequences/Sequences/SCDocument.h
@@ -0,0 +1,13 @@
+//
+//  SCDocument.h
+//  Sequences
+//
+//  Created by Sang Chul Choi on 4/27/13.
+//  Copyright (c) 2013 Sang Chul Choi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SCDocument : NSDocument
+
+@end
---
+++ Sequences/Sequences/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Sequences
+//
+//  Created by Sang Chul Choi on 4/27/13.
+//  Copyright (c) 2013 Sang Chul Choi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
